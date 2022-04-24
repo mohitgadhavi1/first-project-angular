aeec021e9d41b22a92ae4d20b93d8e2b4c2b37ff
Mohit <mohitgadhavi1@gmail.com>
2022-04-24 12:29:11 +0530

    reg

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {TASKS} from '../../mock-task'
+import {Task} from '../../Task'
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.scss']
 })
 export class TaskComponent implements OnInit {
+  tasks: Task[]=[]
 
-  constructor() { }
+  constructor(private taskService:TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>this.tasks=tasks)
   }
 
 }
